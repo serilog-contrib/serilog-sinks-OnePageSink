73e788c3a8a74bc5bf38109d05f3a3a73cf34878
Iskander <nomail86@gmail.com>
2019-05-01 15:34:16 +0300

    Fixed unit tests

--- tests/UnitTests/StaticFileMiddlewareTests.cs
+++ tests/UnitTests/StaticFileMiddlewareTests.cs
@@ -17,18 +17,15 @@ namespace UnitTests
         public async Task Invoke_JavascriptFile_FileContents()
         {
             // Arrange
-            var fileSystemMock = new Mock<IFileSystemManager>();
-            fileSystemMock.Setup(x => x.Exists(It.IsAny<string>())).Returns(true);
             var stream = new MemoryStream(Encoding.ASCII.GetBytes("test"));
-            fileSystemMock.Setup(x => x.GetFileStream(It.IsAny<string>())).Returns(stream);
             IOptions<OnePageSinkOptions> options = Options.Create(new OnePageSinkOptions());
-            var middleware = new StaticFileMiddleware((innerHttpContext) => throw new InvalidOperationException(), options, fileSystemMock.Object);
+            var middleware = new StaticFileMiddleware((innerHttpContext) => throw new InvalidOperationException(), options);
 
             var context = new DefaultHttpContext();
-            context.Request.Path = "/onepage/js/myService.js";
+            context.Request.Path = "/onepage/index.html";
 
             //Act
-            //await middleware.Invoke(context); 
+            await middleware.Invoke(context); 
             //var reader = new StreamReader(context.Response.Body);
             //string streamText = await reader.ReadToEndAsync();
 
